2256cfc4b9557d4850d3fa3cc9877381aa4f2830
lukasmki <lukasarus@gmail.com>
2025-11-13 12:28:31 -0800

    Update to Python 3.13 and uv build system

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-      packages = find_packages(),
-      package_data={'qforce': ['data/*']},
-      entry_points = {
-        'console_scripts': ['qforce=qforce.cli:run',]
-      },
-     )
